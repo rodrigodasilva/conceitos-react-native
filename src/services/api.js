852931b3ec7029ca342185e5cc0fdeb0ef616ba7
rodrigodasilva <ribeiro.rodrigodasilva>
2020-04-17 13:52:33 -0300

    feat: list of repositories

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://10.0.3.2:3333", //Genymotion
 });
 
 export default api;
+
+/**
+ * IOS com Emulador: localhost
+ * IOS com Fisico: IP da máquina
+ * Android com emulador: localhost (adb reverse)
+ * Android com emulador: 10.0.2.2 (Andoid Studio)
+ * Android com emulador: 10.03.2 (Genymotion)
+ * Android com fisico: IP da máquina
+ *
+ */
